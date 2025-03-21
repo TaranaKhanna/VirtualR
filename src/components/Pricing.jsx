b48096b6139ce9ce4dd938b55ff9ea76ef18ca7a
Tarana Khanna <taranakhanna5@gmail.com>
2025-03-21 17:31:57 +0530

    first commit

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,41 @@
+import { CheckCircle2 } from "lucide-react"
+import { pricingOptions } from "../constants"
+
+const Pricing = () => {
+  return (
+    <div className='mt-20'>
+        <h2 className='text-3xl sm:text-5xl lg:text-6xl tracking-wider my-8'>Pricing</h2>
+        <div className="flex flex-wrap">
+            {pricingOptions.map((option, index) => (
+                <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
+                    <div className="p-10 border border-neutral-700 rounded-xl">
+                        <p className="text-4xl mb-8">{option.title}
+                            {option.title === "Pro" && (
+                                <span className="bg-gradient-to-r from-orange-500 to-red-400 text-transparent bg-clip-text text-xl mb-4 ml-2">(Most Popular)</span>
+                            )}
+                        </p>
+                        <p className="mb-8">
+                            <span className="text-5xl mr-2">{option.price}</span>
+                            <span className="text-neutral-400 tracking-tight">/Month</span>
+                        </p>
+                        <ul>
+                            {
+                                option.features.map((feature, i) => (
+                                    <li key={i} className="flex mt-8 items-center">
+                                        <CheckCircle2 />
+                                        <span className="ml-3">{feature}</span>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                        <a href="#" className="inline-flex justify-center items-center w-full text-center h-12 mt-20 tracking-tight text-xl hover:bg-orange-900 border border-orange-900 rounded-lg transition duration-200">
+                            Subscribe</a>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Pricing
